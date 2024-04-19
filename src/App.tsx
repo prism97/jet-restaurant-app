5ce52435e668b1d2cf9647ae274c55410c1120b9
prism97 <priyetasaha@gmail.com>
2024-04-19 12:05:29 +0200

    Add option to select postcode

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,35 @@ import './styles/App.css';
 import Restaurant from './components/Restaurant';
 import RestaurantProps from './components/Restaurant.types';
 import { useState, useEffect } from 'react';
+import postcodes from './postcodes';
 
 
 function App() {
   const [restaurants, setRestaurants] = useState<RestaurantProps[]>([]);
+  const [postalCode, setPostalCode] = useState<string>(postcodes[0]);
 
-  const postcode = 'EC4M7RF';
-  const url = `restaurants/enriched/bypostcode/${postcode}`;
-
-  const fetchRestaurantData = async () => {
-    const response = await fetch(url);
-    const jsonResponse = await response.json();
-    setRestaurants(jsonResponse.restaurants.slice(0, 10));
-  };
+  const url = 'restaurants/enriched/bypostcode/';
 
   useEffect(() => {
+    const fetchRestaurantData = async () => {
+      const response = await fetch(url + postalCode);
+      const jsonResponse = await response.json();
+      setRestaurants(jsonResponse.restaurants.slice(0, 10));
+    };
     fetchRestaurantData();
 
     return () => setRestaurants([]);
-  }, []);
+  }, [postalCode]);
 
   return (
     <div className="App">
+      <label htmlFor='pc'>Select your postcode</label>
+      <select id='pc' value={postalCode}  onChange={(e) => setPostalCode(e.target.value)}>
+        {postcodes.map(
+          (postcode, index) => <option key={index} value={postcode}>{postcode}</option>
+        )}
+      </select>
+      <h2>Restaurants near postcode "{postalCode}"</h2>
       {restaurants.map((restaurant, index) => <Restaurant key={index} {...restaurant} />)}
     </div>
   );
